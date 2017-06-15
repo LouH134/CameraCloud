1c78b3d56f06ed28946e898c2b59b25274396908
Louis <Lharris134@gmail.com>
2017-06-15 17:54:31 -0400

    added signup and login vc

---
+++ CloudCamera/CloudCamera/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  CloudCamera
+//
+//  Created by Louis Harris on 6/15/17.
+//  Copyright © 2017 Louis Harris. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+ 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ CloudCamera/CloudCamera/SignUpVC.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpVC.swift
+//  CloudCamera
+//
+//  Created by Louis Harris on 6/15/17.
+//  Copyright © 2017 Louis Harris. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func dismissSignup(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
